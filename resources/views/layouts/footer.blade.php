1f12e8795fc391d1f9e19179c2771af9a62effca
Alexandro11 <alexandroyule@gmail.com>
2024-04-26 20:12:54 -0500

    Organizando Dashboard y Navigation

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="bg-dark text-white py-4" style="bottom: 0; width: 100%;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <span>© 2024 SACV-APP - Proyecto Laravel</span><br>
+                <span>Authors: Alexandro Yule Valencia - Luis Alberto Donado Terront- S5AN</span><br>
+                <span>
+                    <i class="fab fa-whatsapp"></i>
+                    <a href="https://api.whatsapp.com/send?phone=573157298442" class="ms-2 text-white" target="__blank">+57 3157298442</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</footer>
