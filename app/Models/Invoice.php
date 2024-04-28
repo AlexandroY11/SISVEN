10f9ad7d93d8cba0e530bfcc8c9ac5e0c9b68201
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-04-27 20:17:52 -0500

    Invoice Modelo

---
+++ app/Models/Invoice.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'number',
+        'customer_id',
+        'date',
+        'paymode_id',
+    ];
+
+    /**
+     * Get the customer that owns the invoice.
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    /**
+     * Get the paymode that owns the invoice.
+     */
+    public function paymode()
+    {
+        return $this->belongsTo(Paymode::class);
+    }
+
+    /**
+     * Get the details for the invoice.
+     */
+    public function details()
+    {
+        return $this->hasMany(Detail::class);
+    }
+}
