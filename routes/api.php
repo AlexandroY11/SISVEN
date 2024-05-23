87a89ce5ebfae093218d1262f00cb79a834e0b87
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 18:18:19 -0500

    rutas de las api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\CustomerController;
+use App\Http\Controllers\api\PaymodeController;
+use App\Http\Controllers\api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/customers', CustomerController::class);
+Route::apiResource('/paymodes', PaymodeController::class);
+Route::apiResource('/products', ProductController::class);
--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource("paymodes", PaymodeController::class);
-    Route::resource("customers", CustomerController::class);
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//     Route::resource("paymodes", PaymodeController::class);
+//     Route::resource("customers", CustomerController::class);
     
-    Route::resource('products', ProductController::class);
-    Route::resource('categories', CategoryController::class);
-});
+//     Route::resource('products', ProductController::class);
+//     Route::resource('categories', CategoryController::class);
+// });
 
 
 require __DIR__.'/auth.php';
