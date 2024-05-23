ceb3e3bd7603bb4676971d0f1024c5f91a699cd7
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 18:37:14 -0500

    API ProductController

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -12,7 +14,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = DB::table('products')
+            ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->select('products.*', 'categories.name')
+            ->get();
+
+        return json_encode(compact('products'));
     }
 
     /**
@@ -20,30 +27,62 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        return json_encode(compact('product'));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $product = Product::find($id);
+        
+        if(is_null($product)){
+            return abort(404);
+        }
+
+        $categories = DB::table('categories')
+            ->orderBy('name')
+            ->get();
+
+        return json_encode(compact('product', 'categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->category_id = $request->category_id;
+        $product->save();
+
+        return json_encode(compact('product'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        $product->delete();
+
+        $products = DB::table('products')
+            ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->select('products.*', 'categories.name')
+            ->get();
+
+            return json_encode([ 'products' => $products, 'success' => true]);
     }
 }
